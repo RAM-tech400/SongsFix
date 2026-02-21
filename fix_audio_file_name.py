547e24a4aa12612d91c52e96abc3ddb41cc80df5
Reza Alimohammadi <newram098@gmail.com>
2026-02-21 20:23:50 +0330

    Add help for showing when nothing passed from command line.

--- fix_audio_file_name.py
+++ fix_audio_file_name.py
@@ -6,6 +6,22 @@ import mutagen
 
 song_title = "Unknow"
 
+def print_help():
+    help_text = """
+Usage: fix_audio_file_name.py [OPTIONS] FILE
+
+Show audio information for the specified audio file.
+
+Positional arguments:
+  FILE                  Path to the audio file
+
+Optional arguments:
+  --song-info           Showing song information in output.
+  -h, --help            Show this help message and exit.
+"""
+    print(help_text)
+
+
 def get_song_title(audio):
     if audio.tags:
         song_title = audio.tags.get("TIT2") or audio.tags.get("\xa9nam")
@@ -36,6 +52,8 @@ def main():
     args = parser.parse_args()
     if args.song_info:
         show_audio_info(args.file)
+    else:
+        print_help()
 
 if __name__ == "__main__":
     main()
